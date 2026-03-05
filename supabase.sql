d3945816c13a87f6f8ef479ce84f1da27808ba43
Shree-Pranav-git <tinkuspp142006@gmail.com>
2026-03-05 10:38:30 +0530

    Delete supabase.sql

--- supabase.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE appointments (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    phone VARCHAR(20) NOT NULL,
-    status TEXT NOT NULL DEFAULT 'WAITING',
-    created_at TIMESTAMPTZ DEFAULT NOW()
-);
-
